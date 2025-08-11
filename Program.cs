1efa819e25d9f095d20ee2667cfbc678ff3be595
Swetony <swetonyancelmo@gmail.com>
2025-08-11 19:57:57 -0300

    finalizando projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("123456789", "iPhone 13", "123456789012345", 128);
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "987654321098765", 32);
+
+iphone.InstalarAplicativo("WhatsApp");
+nokia.InstalarAplicativo("Snake");
+
